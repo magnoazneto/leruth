a0193e3901289c4a6f4ade0dacfeee429a181294
Magno Azevedo <magnoazneto@gmail.com>
2019-10-06 21:36:32 -0300

    alterações de script do Orion: animation_loop

--- scripts/Orion.gd
+++ scripts/Orion.gd
@@ -4,10 +4,13 @@ const WALK_SPEED = 200
 const RUN_SPEED = 400
 
 var movedir = Vector2(0,0)
+var aim = Vector2()
 
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
+	animation_loop()
+	
 
 func controls_loop():
 	var LEFT = Input.is_action_pressed("ui_a")
@@ -21,6 +24,26 @@ func controls_loop():
 func movement_loop():
 	var RUN = Input.is_action_pressed("ui_run")
 	var current_speed = 0
-	current_speed = RUN_SPEED if RUN else WALK_SPEED
+	if RUN:
+		global_position = aim
+		current_speed = RUN_SPEED
+		$walkSprite/walkAnim.playback_speed = 4
+	else:
+		current_speed = WALK_SPEED
+		$walkSprite/walkAnim.playback_speed = 2
+
 	var motion = movedir.normalized() * current_speed
 	move_and_slide(motion, Vector2(0,0))
+	
+func animation_loop():
+	if movedir.x > 0:
+		$walkSprite.flip_h = true
+		$walkSprite/walkAnim.play("walk")
+	elif movedir.x < 0:
+		$walkSprite.flip_h = false
+		$walkSprite/walkAnim.play("walk")
+	else:
+		$walkSprite/walkAnim.stop()
+		$walkSprite.frame = 0
+	if movedir.y < 0:
+		pass
